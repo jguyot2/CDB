c5788a3e681a21367eb13fd75b9bdb8deb405915
Julien Guyot <jguyot@excilys.com>
2020-03-12 16:50:37 +0100

    implémentation d'un vrai singleton pour la classe DBConnection

--- cdb/src/main/Main.java
+++ cdb/src/main/Main.java
@@ -3,12 +3,12 @@ package main;
 import ui.CLInterface;
 
 public class Main {
-	
-	//TODO Gestion des exceptions principalement dans la couche service, définitions d'exceptions propres 
 	//TODO faire un vrai Singleton pour la classe DBConnection
+	//TODO Gestion des exceptions principalement dans la couche service, définitions d'exceptions propres 
 	//TODO optionnel : Faire des DTO pour le modèle
 	//TODO optionnel: Créer des interfaces associées à la déstatication de l'interface
 	//TODO RELECTURE ET CORRECTION DES NOMS DE MERDE QUE J'AI MIS
+	//TODO Refaire la javadoc
 	public static void main(String... strings) {
 		CLInterface.start();
 	}
